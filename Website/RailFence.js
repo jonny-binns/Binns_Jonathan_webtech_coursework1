c2ff8144fc55140e4f83b2abd7c0900bb4282e83
Jonathan Binns <40311703@live.napier.ac.uk>
2019-03-01 15:27:55 +0000

    Added: rail fence encoder

--- Website/RailFence.js
+++ Website/RailFence.js
@@ -1,32 +1,25 @@
 function encode(){
     var input = document.getElementById("message").value;
+    var output1 = "";
+    var output2 = "";
+    var output3 = "";
     var output = "";
     try
     {
-        for (var i = 0; i < input.length; )
+        for (var i = 0; i < input.length;)
         {
-          output = output + input.charAt(i);
-            i = i + 4;
+            output1 = output1 + input.charAt(i);
+            i= i + 4;
         }
         for (var i = 1; i< input.length;)
         {
-            output = output + input.charAt(i);
-            i = i + 4;
+            output2 = output2 + input.charAt(i);
+            i= i + 2;
         }
         for (var i = 2; i< input.length;)
         {
-            output = output + input.charAt(i);
-            i = i + 4;
-        }
-        for (var i = 3; i< input.length;)
-        {
-            output = output + input.charAt(i);
-            i = i + 4;
-        }
-        for (var i = 4; i< input.length;)
-        {
-            output = output + input.charAt(i);
-            i = i + 4;
+          output3 = output3 + input.charAt(i);
+          i= i + 4;
         }
     }
     catch
@@ -35,6 +28,6 @@ function encode(){
     }
     finally
     {
-        document.getElementById("output").innerHTML = output;
+        document.getElementById("output").innerHTML = output1 + output2 + output3;
     }
 }
